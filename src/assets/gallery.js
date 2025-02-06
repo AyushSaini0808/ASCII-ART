14daa3ebf0edf75eb7bb27a8c562d3b96f762b71
Ayush Saini <ayushsaini2004@gmail.com>
2025-02-06 21:13:03 +0530

    updated gallery.js

--- src/assets/gallery.js
+++ src/assets/gallery.js
@@ -1,16 +1,16 @@
 export const galleryItems=[
     {
-        image:"/public/saturn.webp",
+        image:"/saturn.webp",
         title : "Saturn",
         description:"Saturn transformed into ASCII"
     },
     {
-        image:"/public/smario.jpg",
+        image:"/smario.jpg",
         title : "Super Mario",
         description:"Mario in the ASCII world"
     },
     {
-        image:"/public/t&j.jpg",
+        image:"/t&j.jpg",
         title : "Tom and Jerry",
         description:"Tom and Jerry captured in ASCII art"
     }
